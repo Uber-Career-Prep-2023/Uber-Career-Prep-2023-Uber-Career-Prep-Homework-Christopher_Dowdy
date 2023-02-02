c90914520559e8cf901bf5b3f6bde9c54f74f459
Christopher Dowdy <cdowdy81@gmail.com>
2023-02-02 11:39:52 -0500

    assignment1-question-3

---
+++ Assignment-1/ZeroSumSubArrays.py
@@ -0,0 +1,28 @@
+'''
+Intuition:
+- Iterate through the whole array, at each index i of arr calcuate the sum of the arr
+up to that index i, add sum to a set
+    - if sum at i == 0 or sum exists already in the set, we have found a subarray
+    that sums to 0
+Technique: One-directional running computation/total
+TC: O(n) where n is size of array
+SC: O(n) where n is size of array
+'''
+def ZeroSumSubArrays(arr):
+    currSum = 0
+    count = 0
+    prevSums = set()
+    for i in range(len(arr)):
+        currSum+=arr[i]
+        if currSum == 0 or currSum in prevSums:
+            count+=1
+        else:
+            prevSums.add(currSum)
+    return count
+
+print(ZeroSumSubArrays([4, 5, 2, -1, -3, -3, 4, 6, -7]))
+print(ZeroSumSubArrays([1, 8, 7, 3, 11, 9]))
+print(ZeroSumSubArrays([8, -5, 0, -2, 3, -4])) #I think this should be 2, not 0
+'''
+Time spent: 50 mins
+'''
