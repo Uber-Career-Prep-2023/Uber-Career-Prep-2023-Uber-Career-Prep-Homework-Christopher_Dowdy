db278988021cafff7ac4eb6adb5f27daf0725574
Christopher Dowdy <cdowdy81@gmail.com>
2023-05-02 19:07:42 -0400

    add delete for BST

--- Assignment-2/BinarySearchTree.py
+++ Assignment-2/BinarySearchTree.py
@@ -39,5 +39,33 @@ class BinarySearchTree():
         else:
             self.root.left = self.insert(self.root.left,val)
         return self.root
+    
+    def delete(self,val):
+        if not self.root:
+            return self.root
+        
+        if self.root.val == val:
+            if not self.root.left:
+                return self.root.right
+            if not self.root.right:
+                return self.root.left
+            # replace root with its successor if it has two children
+            p = self.findSuccessor(self.root)
+            self.root.val = p.val
+            self.root.right = self.delete(self.root.right,p.val)
+            return self.root
+        if self.root.val < val:
+            self.root.right = self.delete(self.root.right, val)
+        else:
+            self.root.left = self.delete(self.root.left, val)
+        
+        return self.root
+
+    def findSuccessor(root):
+        # finds the leftmost child in root's right subtree
+        curr = root.right
+        while curr and curr.left:
+            curr = curr.left
+        return curr
 
         
