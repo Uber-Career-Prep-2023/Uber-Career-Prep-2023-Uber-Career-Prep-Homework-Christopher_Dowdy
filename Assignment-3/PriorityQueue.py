c47bd5454f04bb1bfe693f3166df451eef1f7cfd
Christopher Dowdy <cdowdy81@gmail.com>
2023-06-11 20:50:27 -0500

    completed ds questions

---
+++ Assignment-3/PriorityQueue.py
@@ -0,0 +1,56 @@
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+
+    def top(self):
+        if not self.heap:
+            return None
+        return self.heap[0]
+
+    def insert(self, element, priority):
+        self.heap.append((element, priority))
+        self.heapifyUp()
+
+    def heapifyUp(self):
+        child = len(self.heap) - 1
+        parent = (child - 1) // 2
+
+        while parent >= 0 and self.heap[child][1] > self.heap[parent][1]:
+            self.heap[parent], self.heap[child] = self.heap[child], self.heap[parent]
+            child, parent = parent, (child - 1) // 2
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) > 1:
+            self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        root = self.heap.pop()
+        if self.heap:
+            self.heapifyDown(0)
+        return root
+
+    def heapifyDown(self, i):
+        leftChild = 2 * i + 1
+        rightChild = 2 * i + 2
+        largest = i
+
+        if leftChild < len(self.heap) and self.heap[leftChild][1] > self.heap[largest][1]:
+            largest = leftChild
+        
+        if rightChild < len(self.heap) and self.heap[rightChild][1] > self.heap[largest][1]:
+            largest = rightChild
+
+        if largest != i:
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self.heapifyDown(largest)
+
+pq = PriorityQueue()
+
+pq.insert('A', 2)
+pq.insert('B', 5)
+pq.insert('C', 3)
+
+print(pq.remove())  
+print(pq.remove()) 
+print(pq.remove())  
--- Assignment-3/heap.py
+++ Assignment-3/heap.py
@@ -1,30 +1,49 @@
 class Heap:
     def __init__(self):
         self.heap = []
+
     def top(self):
         return self.heap[0]
+    
     def insert(self,x):
         self.heap.append(x)
+        self.heapifyUp()
 
+    def heapifyUp(self):
         child = len(self.heap)-1
         parent = (child-1)//2
 
         while parent >= 0 and self.heap[parent] > self.heap[child]:
             self.heap[parent],self.heap[child] = self.heap[child],self.heap[parent]
             child, parent = parent, (child-1)//2
-    def remove(self):
-        self.heap[0] = self.heap.pop()
-        self.heapifyDown(0)
-    def heapifyDown(self,i):
-        leftChild = 2*i+1
-        rightChild = 2*i+2
 
-        if leftChild >= len(self.heap) or rightChild >= len(self.heap):
-            return
-        smallestChild = i
-        if self.heap[leftChild] < self.heap[rightChild]:
-            smallestChild = leftChild
-        else:
-            smallestChild = rightChild
-        self.heap[i],self.heap[smallestChild] = self.heap[smallestChild],self.heap[i]
-        self.heapifyDown(smallestChild)
+    def remove(self):
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        root = self.heap.pop()
+        self.heapifyDown(0)
+        return root
+
+    def heapifyDown(self, i):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        smallest = i
+        
+        if left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        
+        if right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+        
+        if smallest != i:
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+            self.heapifyDown(smallest)
+
+    
+h = Heap()
+h.insert(7)
+h.insert(3)
+h.insert(9)
+
+print(h.remove())
+print(h.remove())
+print(h.remove())
